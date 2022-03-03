449d189717c61487add736cff672db5c599654ea
Tay Starr <tstarrroberts@gmail.com>
2022-03-02 20:50:19 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roberto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -55,4 +55,6 @@ var fight = function() {
   window.alert("You need to choose a valid option. Try again!");
 }
 };
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i])
+}
