971bbdc74653165ba517dd356707eb054f76c1ab
Tay Starr <tstarrroberts@gmail.com>
2022-03-02 21:17:21 -0500

    while loop added

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,7 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+  while(enemyHealth > 0) {
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -54,7 +54,10 @@ var fight = function(enemyName) {
 } else {
   window.alert("You need to choose a valid option. Try again!");
 }
+  }
 };
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i])
+var pickedEnemyName = enemyNames[i];
+enemyHealth = 50;
+fight(pickedEnemyName)
 }
